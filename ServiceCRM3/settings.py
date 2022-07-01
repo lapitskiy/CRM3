9a71299dfaa8becbe7b1de291c8f362ba47c4c0d
lapitskiy <lapithome@gmail.com>
2022-07-02 00:55:04 +0300

    upd

--- ServiceCRM3/settings.py
+++ ServiceCRM3/settings.py
@@ -145,6 +145,10 @@ MEDIA_URL = 'media/'
 INTERNAL_IPS = ['127.0.0.1',]
 
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #
 # CKEDITOR
