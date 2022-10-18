ca816794ec53358cdbffa15789be54d0ae3ff1cb
lapitskiy <lapithome@gmail.com>
2022-10-18 03:35:33 +0300

    upd pc

--- users/views.py
+++ users/views.py
@@ -56,7 +56,7 @@ def user_login(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('home')
+            return redirect('orders_home')
     else:
         form = UserLoginForm()
 
